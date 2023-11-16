233395737e51a45adb4e26ba2e73cc88fa916292
soLobster <dhwnd0526@gmail.com>
2023-11-16 19:17:56 +0900

    jsp setting on mac

---
+++ lab05_jsp/src/main/java/com/itwill/jsp2/controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.itwill.jsp2.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "homeController", urlPatterns = { "" })
+public class HomeController extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        System.out.println("homeController::doGet() Call");
+
+        request.getRequestDispatcher("/WEB-INF/home.jsp").forward(request, response);
+        ;
+    }
+
+}
