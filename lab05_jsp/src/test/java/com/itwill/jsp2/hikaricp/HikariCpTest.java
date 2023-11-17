a23406ac9d58595c4cc5174d426a3d3c07f306a4
soLobster <dhwnd0526@gmail.com>
2023-11-17 14:53:02 +0900

    HikariCpTest.java [this -> thin 수정]

--- lab05_jsp/src/test/java/com/itwill/jsp2/hikaricp/HikariCpTest.java
+++ lab05_jsp/src/test/java/com/itwill/jsp2/hikaricp/HikariCpTest.java
@@ -1,5 +1,8 @@
 package com.itwill.jsp2.hikaricp;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -11,14 +14,14 @@ public class HikariCpTest {
     private static final Logger log = LoggerFactory.getLogger(HikariCpTest.class);
     
     @Test
-    public void test() {
+    public void test() throws SQLException{
         // HikariCP(커넥션 풀) 환경 설정을 위한 객체 생성:
         HikariConfig config = new HikariConfig();
         
         // HikariCP 환경 설정:
         config.setDriverClassName("oracle.jdbc.OracleDriver");
         
-        config.setJdbcUrl("jdbc:oracle:this:@localhost:1521:xe");
+        config.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:xe");
         config.setUsername("scott");
         config.setPassword("tiger");
         
@@ -29,5 +32,15 @@ public class HikariCpTest {
         Assertions.assertNotNull(ds);
         log.debug("ds={}", ds);
         
+        //커넥션 풀(데이터 소스)에서 커넥션 객체를 빌려옴.
+        Connection conn = ds.getConnection();
+        
+        //커넥션 객체는 null이 아니어야 함.
+        Assertions.assertNotNull(conn);
+        log.debug("conn={}", conn);
+        
+        //사용이 끝난 커넥션 객체는 풀에 반환을 해야한다.
+        conn.close(); // close: DB 서버와의 물리적인 연결을 끊는게 아니라 커넥션 풀에 반환!
+        log.debug("커넥션 반환 성공");
     }
 }
