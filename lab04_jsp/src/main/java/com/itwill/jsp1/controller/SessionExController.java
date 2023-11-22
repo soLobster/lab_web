fc7caf3b598d43df7e9ccad79004074a16b52561
soLobster <dhwnd0526@gmail.com>
2023-11-22 17:45:55 +0900

    23 11 22 Session

--- lab04_jsp/src/main/java/com/itwill/jsp1/controller/SessionExController.java
+++ lab04_jsp/src/main/java/com/itwill/jsp1/controller/SessionExController.java
@@ -5,6 +5,8 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
 
 /**
@@ -19,6 +21,14 @@ public class SessionExController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("sessionExController::doGet() 호출");
+		
+		// 서버가 저장하는 클라이언트마다 매핑되어 있는 세션 객체를 찾음.
+		// 1) 클라이언트가 JSESSIONID 쿠키를 보내면, 세션 아이디를 사용해서 세션 객체를 찾음.
+		// 2) 클라이언트가 보낸 쿠키에 세션 아이디가 없는 경우에는 새로운 세션 객체를 생성.
+		HttpSession session = request.getSession();
+		System.out.println(session);
+		
+		session.setAttribute("nickname", "관리자");
 		
 		request.getRequestDispatcher("/WEB-INF/session.jsp").forward(request, response);
 	}
--- lab04_jsp/src/main/webapp/WEB-INF/session.jsp
+++ lab04_jsp/src/main/webapp/WEB-INF/session.jsp
@@ -14,6 +14,8 @@
 	<body>
 		<h1>Session</h1>
 		
+        <h2>${nickname}님, 안녕하세요</h2>
+        
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
