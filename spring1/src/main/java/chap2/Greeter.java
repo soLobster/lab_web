96c468a71ef24830b8b7da012be44002ed8e32ba
soLobster <dhwnd0526@gmail.com>
2023-11-29 09:46:32 +0900

    초보 웹 개발자를 위한 스프링 5 프로그래밍 입문

---
+++ spring1/src/main/java/chap2/Greeter.java
@@ -0,0 +1,15 @@
+package chap2;
+
+public class Greeter {
+
+    private String format;
+    
+    public String greet(String guest) {
+        return String.format(format, guest);
+    }
+    
+    public void setFormat(String format) {
+        this.format=format;
+    }
+    
+}
